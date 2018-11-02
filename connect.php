b8e6dfa46ae29cd92d15d8f3bf31fff7b6694045
Adam Morelli <amorelli@gmail.com>
2018-11-02 16:15:50 -0400

    Updated database information

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
 
-$server = 'server';
-$username   = 'username';
-$password   = 'password';
-$database   = 'database';
+$server = 'us-cdbr-iron-east-01.cleardb.net';
+$username   = 'b28993a128cef3';
+$password   = 'aab1e67d';
+$database   = 'heroku_8b24e95e0b7a1d0';
 
-$dbc = mysqli_connect('server','username','password');
-mysqli_select_db($dbc, 'database');
+$dbc = mysqli_connect('us-cdbr-iron-east-01.cleardb.net','b28993a128cef3','aab1e67d');
+mysqli_select_db($dbc, 'heroku_8b24e95e0b7a1d0');
 
 if(!mysqli_connect($server, $username, $password, $database))
 {
